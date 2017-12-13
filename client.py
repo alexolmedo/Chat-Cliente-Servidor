a96f64e5073575b62edbab6b7b07633d9e11ae12
alexander <olmedo2408@gmail.com>
2017-12-13 11:37:55 -0500

    Commit inicial agregado cliente y servidor

---
+++ client.py
@@ -0,0 +1,32 @@
+# coding=utf-8
+import sys
+import socket
+import time
+import thread
+
+HOST = '127.0.0.1'
+RECV_BUFFER = 1024
+PORT = 9000
+
+def send_message(socket, message):
+    message = raw_input()
+    socket.send(message)
+    #time.sleep(5)
+    send_message(socket,message)
+
+
+def client():
+    addr = (HOST, PORT)
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    #client_socket.settimeout(30)
+    client_socket.connect(addr)
+
+    thread.start_new_thread(send_message, (client_socket,""))
+
+    while 1:
+
+        data = client_socket.recv(RECV_BUFFER)
+        print data
+
+if __name__ == "__main__":
+    sys.exit(client())
