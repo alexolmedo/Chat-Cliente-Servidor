dadacecf07dfb8e43695dde65eac06a827f7557d
alexander <olmedo2408@gmail.com>
2017-12-14 12:17:19 -0500

    Mostrar mensajes en el primer peer

--- p2pchat.py
+++ p2pchat.py
@@ -7,7 +7,7 @@ import thread
 HOST = '127.0.0.1'
 SOCKET_LIST = []
 RECV_BUFFER = 1024
-PORT = 9000
+PORT = 9001
 
 def send_message(socket, message):
     message = raw_input()
@@ -20,7 +20,7 @@ class Client:
 
 def manager(client):
 
-    print client.code + " conectado desde: ", client.addr
+    #print client.code + " conectado desde: ", client.addr
 
     # Este bucle se repite cada vez que se encuentran datos en el bufer
     while 1:
@@ -30,6 +30,7 @@ def manager(client):
             #Comprobar si se trata de un mensaje privado
             if ':' in str(data):
                 recipient, msg = data.split(":")
+                print client.code + ": " + msg
                 for c in SOCKET_LIST:
                      if c.code == recipient:
                         if not c.socket.send(client.code + ": " + msg):
@@ -43,6 +44,7 @@ def manager(client):
 
             # Hacemos un broadcast de lo que dice el cliente a todos los demás
             # clientes
+            print client.code + ": " + str(data)
             for c in SOCKET_LIST:
                 if not c.socket.send(client.code + ": " + str(data)):
                     try:
@@ -52,7 +54,7 @@ def manager(client):
                         print "Cliente " + c.code + " no encontrado - Socket cerrado"
                         break
 
-            print "Clientes totales: " + str(len(SOCKET_LIST))
+            #print "Clientes totales: " + str(len(SOCKET_LIST))
 
         except Exception as e:
             print "Excepcion en socket de cliente:"
@@ -65,26 +67,6 @@ def manager(client):
                 break
     return
 
-def server():
-
-    server_addr = (HOST, PORT)
-    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    server_socket.bind(server_addr)
-    server_socket.listen(10)
-
-    print "Server is listening for connections\n"
-    while 1:
-        c = Client()
-        c.socket, c.addr = server_socket.accept()
-        c.code = c.socket.recv(RECV_BUFFER)
-        SOCKET_LIST.append(c)
-
-        thread.start_new_thread(manager, (SOCKET_LIST[-1],))
-
-    server_socket.close()
-
-
 def client():
     addr = (HOST, PORT)
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -93,7 +75,7 @@ def client():
     try: 
         client_socket.connect(addr)
     except:
-        print 'No hay nadie conectado'
+        print 'No hay nadie conectado, usted es el primero en la red'
         server()
         return
 
@@ -103,6 +85,26 @@ def client():
     while 1:
         data = client_socket.recv(RECV_BUFFER)
         print data
+
+def server():
+
+    server_addr = (HOST, PORT)
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    server_socket.bind(server_addr)
+    server_socket.listen(10)
+
+    while 1:
+        c = Client()
+        c.socket, c.addr = server_socket.accept()
+        c.code = c.socket.recv(RECV_BUFFER)
+        SOCKET_LIST.append(c)
+        thread.start_new_thread(manager, (SOCKET_LIST[-1],))
+
+    server_socket.close()
+
+
+
 
 if __name__ == "__main__":
     sys.exit(client())
